95216ddbb4f149b39dfa287a7eb92a6a09f35e49
Clayton Roth <croth0627@yahoo.com>
2017-01-03 20:56:37 -0800

    Refactored Enumerable Travering
    * `EnumerableTraverser` moved to `Traversable<T>.EnumerableSequence`
      * Now `internal`.

--- Bogosoft.Collections.Async/Traversable.cs
+++ Bogosoft.Collections.Async/Traversable.cs
@@ -239,6 +239,88 @@ namespace Bogosoft.Collections.Async
             }
         }
 
+        internal struct EnumerableSequence : ITraversable<T>
+        {
+            internal struct Cursor : ICursor<T>
+            {
+                private IEnumerator<T> enumerator;
+
+                private bool active;
+
+                public bool IsDisposed { get; private set; }
+
+                public bool IsExpended { get; private set; }
+
+                public Cursor(IEnumerator<T> enumerator)
+                {
+                    active = false;
+
+                    this.enumerator = enumerator;
+
+                    IsDisposed = false;
+                    IsExpended = false;
+                }
+
+                public void Dispose()
+                {
+                    if (!IsDisposed)
+                    {
+                        enumerator.Dispose();
+
+                        IsDisposed = true;
+                    }
+                }
+
+                public Task<T> GetCurrentAsync(CancellationToken token)
+                {
+                    token.ThrowIfCancellationRequested();
+
+                    if (!active)
+                    {
+                        throw new InvalidOperationException("This cursor has not been initialized.");
+                    }
+
+                    return Task.FromResult(enumerator.Current);
+                }
+
+                public Task<bool> MoveNextAsync(CancellationToken token)
+                {
+                    if (!token.IsCancellationRequested && !IsExpended)
+                    {
+                        if (false == (active = enumerator.MoveNext()))
+                        {
+                            IsExpended = true;
+                        }
+                    }
+
+                    return Task.FromResult(active);
+                }
+            }
+
+            private IEnumerable<T> enumerable;
+
+            public EnumerableSequence(IEnumerable<T> enumerable)
+            {
+                this.enumerable = enumerable;
+            }
+
+            public Task<ICursor<T>> GetCursorAsync(CancellationToken token)
+            {
+                ICursor<T> cursor;
+
+                if (token.IsCancellationRequested)
+                {
+                    cursor = Cursor<T>.Empty;
+                }
+                else
+                {
+                    cursor = new Cursor(enumerable.GetEnumerator());
+                }
+
+                return Task.FromResult(cursor);
+            }
+        }
+
         /// <summary>
         /// Get an empty traversable sequence.
         /// </summary>
