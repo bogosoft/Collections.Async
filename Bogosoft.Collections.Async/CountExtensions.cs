b79fd1a59bf77dcd51c77dbc0df0a21c49d660d6
Clayton Roth <croth0627@yahoo.com>
2017-01-03 23:00:09 -0800

    Added `ICount` contract and extension methods.

---
+++ Bogosoft.Collections.Async/CountExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bogosoft.Collections.Async
+{
+    /// <summary>
+    /// Extended functionality for the <see cref="ICount{T}"/> contract.
+    /// </summary>
+    public static class CountExtensions
+    {
+        /// <summary>
+        /// Count the number of objects of the specified type. Calling this method is equivalent to calling
+        /// <see cref="ICount{T}.CountAsync(CancellationToken)"/> with a value of
+        /// <see cref="CancellationToken.None"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the objects that can be counted.</typeparam>
+        /// <param name="target">The current <see cref="ICount{T}"/> implementation.</param>
+        /// <returns>
+        /// A value corresponding to the number of objects the specified tpye.
+        /// </returns>
+        public static async Task<ulong> CountAsync<T>(this ICount<T> target)
+        {
+            return await target.CountAsync(CancellationToken.None);
+        }
+    }
+}
